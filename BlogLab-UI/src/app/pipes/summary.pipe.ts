700f1461bc9651e1076b9aaba03ef144f0d44b00
Seung <hhwannn2@gmail.com>
2024-09-03 23:43:11 -0500

    create pipe for content

---
+++ BlogLab-UI/src/app/pipes/summary.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'summary',
+  standalone: true
+})
+export class SummaryPipe implements PipeTransform {
+
+  transform(content: string, characterLimit: number): string {
+    if (content.length <= characterLimit) {
+      return content;
+    } else {
+      return `${content.substring(0, characterLimit)}...`;
+    }
+  }
+
+}
